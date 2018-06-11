a2b952f5e0f2487710e0911153c9a0cc26cdf0ad
miw-upm <j.bernal@upm.es>
2018-06-11 05:17:16 +0200

    add dispatcher

---
+++ src/main/java/api/Dispatcher.java
@@ -0,0 +1,40 @@
+package api;
+
+import api.apiControllers.UserApiController;
+import api.dtos.UserDto;
+import api.exceptions.ArgumentNotValidException;
+import api.exceptions.RequestInvalidException;
+import http.HttpRequest;
+import http.HttpResponse;
+import http.HttpStatus;
+
+public class Dispatcher {
+
+    public void submit(HttpRequest request, HttpResponse response) {
+        String ERROR_MESSAGE = "{'error':'%S'}";
+        try {
+            switch (request.getMethod()) {
+                case POST:
+                    throw new RequestInvalidException("method error: " + request.getMethod());
+                case GET:
+                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                case PUT:
+                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                case PATCH:
+                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                case DELETE:
+                    throw new RequestInvalidException("request error: " + request.getMethod() + ' ' + request.getPath());
+                default:
+                    throw new RequestInvalidException("method error: " + request.getMethod());
+            }
+        } catch (ArgumentNotValidException | RequestInvalidException exception) {
+            response.setBody(String.format(ERROR_MESSAGE, exception.getMessage()));
+            response.setStatus(HttpStatus.BAD_REQUEST);
+        } catch (Exception exception) {  // Unexpected
+            exception.printStackTrace();
+            response.setBody(String.format(ERROR_MESSAGE, exception));
+            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
